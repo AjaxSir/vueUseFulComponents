0174a33100c00e029fb072ec1d16325cbc7a5516
AjaxSir <745284215@qq.com>
2018-09-12 15:31:37 +0800

    add: 静动态路由传参

---
+++ routerParams.vue
@@ -0,0 +1,13 @@
+1 query 传递参数
+(1) 路由为静态路由
+发送参数
+ <router-link :to="{ path:'/main/child', query: { name: 'haha'} }"> 跳转</router-link>
+ 接受参数
+ <h3>{{ this.$route.query.name }}</h3> 想当于url拼接参数
+ 2 params 
+ (1) 路由为动态路由
+path: "/main/child/:name",
+<router-link to="/main/child/haha"> 跳转</router-link>  
+<router-link :to="{ name: 'child', params: { name: 'haha' }}"> 跳转</router-link>
+接收参数
+this.$route.params.name
