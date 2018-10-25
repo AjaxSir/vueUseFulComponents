29593477859979ff43f2afaeccc388abcb625a77
AjaxSir <745284215@qq.com>
2018-10-25 09:55:42 +0800

    add:vue复制到剪贴板

---
+++ 复制到剪贴板.vue
@@ -0,0 +1,8 @@
+copy () {
+      document.addEventListener('copy', e => {
+        e.clipboardData.setData('text/plain', this.addr)
+        e.preventDefault() // 阻止默认行为
+      })
+      document.execCommand('copy') // 使文档处于可编辑状态，否则无效
+      this.$toast('已复制到剪贴板')
+    }
