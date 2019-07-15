55c91c0403ad219e6c8b021b65b6db5511c66f0f
AjaxSir <745284215@qq.com>
2019-07-15 15:16:19 +0800

    更新 + vueCli_3.x 实践

---
+++ vue_cli_3.X/vue3x-demo/src/components/ListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span>2</span>
+    <span v-for="(item, index) in list" :key="index">{{ item }}</span>
+    <br />
+    <span>{{ count }}</span>
+    <el-button @click="add">add</el-button>
+    <br />
+    <span v-for="(ele, index) in countList" :key="'ele.id' + index">{{
+      ele.title
+    }}</span>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Emit, Prop } from "vue-property-decorator";
+
+@Component
+export default class ListItem extends Vue {
+  @Prop() private countList!: any[];
+  @Emit("fromSon") send(val: string) {}
+  private list: number[] = [1, 2, 4];
+  private count: number = 1;
+  add() {
+    this.count += 1;
+    this.send(`${this.count}+son`);
+  }
+}
+</script>
---
+++ vue_cli_3.X/vue3x-demo/src/views/product.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <list-item :countList="countList" @fromSon="faEmit"></list-item>
+    <span>{{ msg }}</span>
+    <span v-for="(ele, index) in countList" :key="index">{{ ele.title }}</span>
+  </div>
+</template>
+<script lang="ts">
+import ListItem from "@/components/ListItem.vue";
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {
+    ListItem
+  }
+})
+export default class product extends Vue {
+  private msg: string = `father's text`;
+  private count: number = 1;
+  private countList: any[] = [
+    {
+      title: "first",
+      id: 1
+    }
+  ];
+  faEmit(msg1: string) {
+    this.msg = msg1;
+  }
+}
+</script>
